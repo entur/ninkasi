d5d063158334769d57e3d3bc4153d6b9b1a771a6
Sean Scully <sean.erik.scully@gmail.com>
2016-09-12 11:32:05 +0200

    disabled loggerMiddleware

--- store/store.js
+++ store/store.js
@@ -4,7 +4,7 @@ import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import * as reducers from '../reducers'
 
-const loggerMiddleware = createLogger()
+//const loggerMiddleware = createLogger()
 
 const initialState = {}
 
@@ -17,8 +17,7 @@ export default function configureStore(history) {
     combinedReducer,
     initialState,
     applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
+      thunkMiddleware
     )
   )
 }
