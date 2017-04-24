568704a924fced65306cd7c5cae6cdb960eb0501
Sean Scully <sean.erik.scully@gmail.com>
2017-04-24 15:54:05 +0200

    use position:fixed for modal dialog to prevent adjustments to background width

--- modals/Modal.js
+++ modals/Modal.js
@@ -36,13 +36,13 @@ class Modal extends React.Component {
     }
 
     let backdropStyle = {
-      position: 'absolute',
+      position: 'fixed',
       width: '100%',
       height: '100%',
       top: 0,
       left: 0,
       zIndex: '10',
-      background: 'rgba(0, 0, 0, 0.3)'
+      background: 'rgba(0, 0, 0, 0.3)',
     }
 
     if (this.props.backdropStyle) {
