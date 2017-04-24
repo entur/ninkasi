6a90579ca68a9a965c88e3e57d6e2d87f2c58c62
Sean Scully <sean.erik.scully@gmail.com>
2017-04-24 09:14:46 +0200

    NRP-1395 update token parser with adminEditRouteData role

--- roles/rolesParser.js
+++ roles/rolesParser.js
@@ -20,15 +20,20 @@ rolesParser.getUserProviders = (tokenParsed, providers) => {
   if (!tokenParsed || !tokenParsed.roles) return []
 
   let allowedOrganisations = []
+  let isAdmin = false
 
   tokenParsed.roles.forEach( roleString => {
     let roleJSON = JSON.parse(roleString)
     if (roleJSON.r === 'editRouteData') {
       allowedOrganisations.push(roleJSON.o)
+    } else if (roleJSON.r === 'adminEditRouteData') {
+      isAdmin = true
     }
   })
 
   let userOrganisations = []
+
+  if (isAdmin) return providers
 
   providers.forEach( org => {
     if (org.sftpAccount && allowedOrganisations.indexOf(org.sftpAccount.toUpperCase()) > -1)
