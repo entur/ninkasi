a5756cd9207ee1f1c49df60a3a39e7855d704f8f
Sean Scully <sean.erik.scully@gmail.com>
2017-07-27 10:29:57 +0200

    minor prettify

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
-import 'babel-polyfill'
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import Keycloak from 'keycloak-js'
-import App from './containers/App'
-import configureStore from './store/store'
-import './sass/main.scss'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import cfgreader from './config/readConfig'
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import Keycloak from 'keycloak-js';
+import App from './containers/App';
+import configureStore from './store/store';
+import './sass/main.scss';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import cfgreader from './config/readConfig';
 
-cfgreader.readConfig( (function(config) {
-  window.config = config
-  authWithKeyCloak(config.endpointBase)
-}).bind(this))
-
+cfgreader.readConfig(
+  function(config) {
+    window.config = config;
+    authWithKeyCloak(config.endpointBase);
+  }.bind(this)
+);
 
 function authWithKeyCloak(endpointBase) {
+  let kc = new Keycloak(endpointBase + 'config/keycloak.json');
 
-  let kc = new Keycloak(endpointBase + 'config/keycloak.json')
+  kc
+    .init({ onLoad: 'login-required', checkLoginIframe: false })
+    .success(authenticated => {
+      if (authenticated) {
+        localStorage.setItem('NINKASI::jwt', kc.token);
 
-  kc.init({ onLoad: 'login-required', checkLoginIframe: false }).success( authenticated => {
+        setInterval(() => {
+          kc.updateToken(10).error(() => kc.logout());
+          localStorage.setItem('NINKASI::jwt', kc.token);
+        }, 10000);
 
-    if (authenticated) {
-
-      localStorage.setItem('NINKASI::jwt', kc.token)
-
-      setInterval(() => {
-        kc.updateToken(10).error(() => kc.logout())
-        localStorage.setItem('NINKASI::jwt', kc.token)
-
-      }, 10000)
-
-      renderIndex(kc)
-
-    } else {
-      kc.login()
-    }
-  })
+        renderIndex(kc);
+      } else {
+        kc.login();
+      }
+    });
 }
 
 function renderIndex(kc) {
-
-  const store = configureStore(kc)
-  injectTapEventPlugin()
+  const store = configureStore(kc);
+  injectTapEventPlugin();
 
   render(
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>,
     document.getElementById('root')
-  )
-
+  );
 }
