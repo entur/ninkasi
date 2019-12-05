a06862021baca0d7cbdd11d4647a1a65c8c3eeb8
Tom Erik Støwer <testower@gmail.com>
2019-12-05 13:55:10 +0100

    Add pelias component to geocoder screen

---
+++ src/screens/geocoder/components/Pelias.js
@@ -0,0 +1,164 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ */
+
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import Checkbox from 'material-ui/Checkbox';
+import Divider from 'material-ui/Divider';
+import peliasTasks from 'config/peliasTasks';
+import moment from 'moment';
+import RaisedButton from 'material-ui/RaisedButton';
+import SuppliersActions from 'actions/SuppliersActions';
+import ConfirmDialog from 'modals/ConfirmDialog';
+
+const initialPeliasOptions = () => {
+  let tasks = {};
+  peliasTasks.forEach(option => (tasks[option.task] = true));
+  return tasks;
+};
+
+const getLabelByJobType = type => {
+  for (let i = 0; i < peliasTasks.length; i++) {
+    if (peliasTasks[i].task === type) return peliasTasks[i].label;
+  }
+};
+
+const getColorByStatus = status => {
+  switch (status) {
+    case 'STARTED':
+      return '#08920e';
+    case 'OK':
+      return '#08920e';
+    case 'FAILED':
+      return '#990000';
+    default:
+      return 'grey';
+  }
+};
+
+const Pelias = ({ otherStatus, getGraphStatus, executePeliasTask }) => {
+  const [peliasOptions, setPeliasOptions] = useState(initialPeliasOptions());
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  useEffect(() => {
+    getGraphStatus();
+  }, []);
+
+  const handlePeliasOptionChecked = (event, task) => {
+    setPeliasOptions(
+      Object.assign({}, peliasOptions, {
+        [task]: event.target.checked
+      })
+    );
+  };
+
+  const handleExecutePelias = () => {
+    setConfirmDialogOpen(true);
+  };
+
+  const confirmExecutePelias = () => {
+    setConfirmDialogOpen(false);
+    executePeliasTask(peliasOptions);
+  };
+
+  return (
+    <div>
+      {peliasTasks.map(option => (
+        <Checkbox
+          key={'pelias-checkbox-' + option.task}
+          label={option.label}
+          onCheck={e => handlePeliasOptionChecked(e, option.task)}
+          defaultChecked={true}
+          labelPosition="right"
+          style={{ marginTop: 5, marginBottom: 5 }}
+        />
+      ))}
+      <Divider
+        key={'pelias-divider1'}
+        style={{ marginTop: 10, marginBottom: 5 }}
+      />
+      <div
+        key={'pelias-options-status-wrapper'}
+        style={{ display: 'flex', flexDirection: 'column', padding: 5 }}
+      >
+        <span style={{ fontWeight: 600 }}>Status</span>
+        {otherStatus.map((status, index) => (
+          <div
+            key={'jobtype-status' + index}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              padding: 2,
+              background: index % 2 ? '#F8F8F8' : '#fff'
+            }}
+          >
+            <div style={{ marginLeft: 5, flex: 9, fontSize: '0.9em' }}>
+              {getLabelByJobType(status.type)}
+            </div>
+            <div style={{ fontSize: '0.9em', flex: 6 }}>
+              {moment(status.started).format('LLLL')}
+            </div>
+            <div
+              style={{
+                marginLeft: 5,
+                flex: 2,
+                color: getColorByStatus(status.status)
+              }}
+            >
+              {status.status}
+            </div>
+          </div>
+        ))}
+      </div>
+      <Divider
+        key={'pelias-divider2'}
+        style={{ marginTop: 10, marginBottom: 10 }}
+      />
+      <div
+        key={'pelias-buttons'}
+        style={{ width: '100%', textAlign: 'center' }}
+      >
+        <RaisedButton
+          primary={true}
+          labelStyle={{ fontSize: 12 }}
+          onClick={() => handleExecutePelias()}
+          disabled={Object.values(peliasOptions).every(value => !value)}
+          label={'Execute'}
+        />
+      </div>
+      <ConfirmDialog
+        open={confirmDialogOpen}
+        handleSubmit={confirmExecutePelias}
+        title="Execute Pelias tasks"
+        info="Are you sure you want to execute selected pelias tasks?"
+        handleClose={() => {
+          setConfirmDialogOpen(false);
+        }}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  otherStatus: state.SuppliersReducer.otherStatus || []
+});
+
+const mapDispatchToProps = dispatch => ({
+  getGraphStatus: () => dispatch(SuppliersActions.getGraphStatus()),
+  executePeliasTask: peliasOptions =>
+    dispatch(SuppliersActions.executePeliasTask(peliasOptions))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Pelias);
--- src/screens/geocoder/index.js
+++ src/screens/geocoder/index.js
@@ -1,5 +1,58 @@
 import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Pelias from './components/Pelias';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
 
 export default () => {
-  return <div>Geocoder</div>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+        style={{ background: 'lightgray' }}
+      >
+        <Tab label="Pelias" {...a11yProps(0)} />
+        <Tab label="OSM POI filter" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Pelias />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        OSM filters
+      </TabPanel>
+    </div>
+  );
 };
