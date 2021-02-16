c57ba78b556ac4a81dcdd81f6517abe789c09982
vincent <vincent.paturet@entur.org>
2021-02-16 12:12:49 +0100

    Renamed role assignments claim

--- src/app/Auth0AuthenticatedApp.js
+++ src/app/Auth0AuthenticatedApp.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const namespace = 'https://ror.api.dev.entur.io/claims/role_assignments';
+const namespace = 'https://ror.entur.io/role_assignments';
 
 export default ({ children }) => {
   const {
