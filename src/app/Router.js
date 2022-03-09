3d8e8cd6b7e34f4ff2ca1268d37834896d0b9c1d
Tom Erik Støwer <testower@gmail.com>
2022-03-09 08:16:34 +0100

    Draft for integrating udug microfrontend

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, useHistory } from 'react-router';
 
 import Providers from 'screens/providers';
 import Geocoder from 'screens/geocoder';
+import { MicroFrontend } from '@entur/micro-frontend';
+import { useAuth } from '@entur/auth-provider';
+
+const FetchStatus = props => {
+  if (props.status !== 'SUCCESS') {
+    return <pre>{JSON.stringify(props)}</pre>;
+  } else {
+    return null;
+  }
+};
 
 export default () => {
+  const auth = useAuth();
   return (
     <Switch>
       <Route exact path="/" component={Providers} />
       <Route exact path="/geocoder" component={Geocoder} />
+      <Route path="/netex-validation-reports">
+        <MicroFrontend
+          id="ror-udug"
+          host="http://localhost:3000"
+          path="/netex-validation-reports"
+          staticPath=""
+          name="NeTEx validation reports"
+          payload={{
+            getToken: auth.getAccessToken
+          }}
+          FetchStatus={FetchStatus}
+          handleError={e => console.log(e)}
+        />
+      </Route>
     </Switch>
   );
 };
