7072c2aebedbff19394aeac45cf60a51c8edda76
Tom Erik Støwer <testower@gmail.com>
2023-01-06 14:30:15 +0100

    Get rid of convict for configuration

--- src/config/readConfig.js
+++ src/config/readConfig.js
@@ -14,33 +14,17 @@
  *
  */
 
-import axios from 'axios';
+import { getEnvironment } from './getEnvironment';
 
-/*
-Reading config json as served out of the node application.
-*/
-
-const configreader = {};
-let config;
+var configreader = {};
 
 configreader.readConfig = callback => {
-  if (config && typeof config !== 'undefined') {
-    callback(config);
-    return;
-  }
-  axios({
-    url: '/config.json',
-    timeout: 2000,
-    method: 'get',
-    responseType: 'json'
-  })
-    .then(function(response) {
-      config = response.data;
-      callback(config);
-    })
-    .catch(function(response) {
-      throw new Error('Could not read config: ' + response);
-    });
+  const fetchEnvConfig = async () => {
+    const env = getEnvironment();
+    const { default: config } = await import(`./environments/${env}.json`);
+    callback(Object.assign({}, config));
+  };
+  fetchEnvConfig();
 };
 
 export default configreader;
