cb5636f7e69675c4a5d380c1bacc149a8f4f5186
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-10-22 13:33:08 +0200

    Adding date.

--- src/screens/providers/components/GraphStatus.js
+++ src/screens/providers/components/GraphStatus.js
@@ -65,7 +65,7 @@ const GraphStatusDetails = ({ title, status, started }) => (
           </span>
         </span>
         <span
-          title={moment(creationDate).format('DD-MM-YYYY hh:mm:ss')}
+          title={moment(started).format('DD-MM-YYYY hh:mm:ss')}
           style={{ fontSize: '0.8em', paddingLeft: 5, whiteSpace: 'nowrap' }}
         >
           {moment(started).fromNow()}
