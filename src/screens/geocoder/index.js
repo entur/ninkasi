176002834dcfb053ec1f270800dd3a2aa9f042c2
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-11-14 12:32:47 +0100

    Upload tariff zones

--- src/screens/geocoder/index.js
+++ src/screens/geocoder/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Pelias from './components/Pelias';
 import OSMPOIFilter from './components/OSMPOIFilter';
+import { TariffZonesImport } from './components/tariffZonesImport/TariffZonesImport';
+import { getQueryVariable } from '../../utils';
+import SuppliersActions from '../../actions/SuppliersActions';
+import { useDispatch } from 'react-redux';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -32,6 +36,11 @@ function a11yProps(index) {
 
 export default () => {
   const [value, setValue] = React.useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(SuppliersActions.getAllProviders());
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -47,12 +56,16 @@ export default () => {
       >
         <Tab label="Geocoder pipeline" {...a11yProps(0)} />
         <Tab label="OSM POI filter" {...a11yProps(1)} />
+        <Tab label="TariffZone/FareZone Import" {...a11yProps(2)} />
       </Tabs>
       <TabPanel value={value} index={0}>
         <Pelias />
       </TabPanel>
       <TabPanel value={value} index={1}>
         <OSMPOIFilter />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <TariffZonesImport />
       </TabPanel>
     </div>
   );
