9a45746cf0bc3bb74f2aabae7120ad1503c36ef7
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-08-04 23:40:12 +0200

    Migrate from CRA to vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Match the baseUrl from jsconfig.json
+      '@': path.resolve(__dirname, './src'),
+      'app': path.resolve(__dirname, './src/app'),
+      'config': path.resolve(__dirname, './src/config'),
+      'store': path.resolve(__dirname, './src/store'),
+      'actions': path.resolve(__dirname, './src/actions'),
+      'reducers': path.resolve(__dirname, './src/reducers'),
+      'modals': path.resolve(__dirname, './src/modals'),
+      'screens': path.resolve(__dirname, './src/screens'),
+      'utils': path.resolve(__dirname, './src/utils'),
+      'models': path.resolve(__dirname, './src/models'),
+      'static': path.resolve(__dirname, './src/static'),
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true
+  },
+});
