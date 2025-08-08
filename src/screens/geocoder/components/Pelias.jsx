31bbf5e365e8dcfb4b5e215f53796bbe79ccd035
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-08-08 14:47:12 +0200

    Fix checboxes in geocoder pipeline tab

--- src/screens/geocoder/components/Pelias.jsx
+++ src/screens/geocoder/components/Pelias.jsx
@@ -17,7 +17,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import withAuth from 'utils/withAuth';
-import { Checkbox, Divider } from '@mui/material';
+import { Checkbox, Divider, FormControlLabel } from '@mui/material';
 import peliasTasks from 'config/peliasTasks';
 import moment from 'moment';
 import Button from '@mui/material/Button';
@@ -77,13 +77,16 @@ const Pelias = ({ otherStatus, dispatch, getToken }) => {
   return (
     <div>
       {peliasTasks.map(option => (
-        <Checkbox
+        <FormControlLabel
           key={'pelias-checkbox-' + option.task}
+          control={
+            <Checkbox
+              onChange={e => handlePeliasOptionChecked(e, option.task)}
+              defaultChecked={true}
+            />
+          }
           label={option.label}
-          onCheck={e => handlePeliasOptionChecked(e, option.task)}
-          defaultChecked={true}
-          labelPosition="right"
-          style={{ marginTop: 5, marginBottom: 5 }}
+          sx={{ display: 'block', mt: 0.625, mb: 0.625 }}
         />
       ))}
       <Divider
